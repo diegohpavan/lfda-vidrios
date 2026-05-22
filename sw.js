91102c4ebbbd3547adcf0d430b88cc0a0f23db7e
diegohpavan <diegohpavan@gmail.com>
2026-05-21 21:17:33 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'lfda-v1';
+const ASSETS = [
+  '/lfda-vidrios/',
+  '/lfda-vidrios/index.html',
+  '/lfda-vidrios/manifest.json',
+  '/lfda-vidrios/icon.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network first para Firebase, cache first para assets
+  if (e.request.url.includes('firebase') || e.request.url.includes('googleapis')) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(cached => cached || fetch(e.request))
+    );
+  }
+});
